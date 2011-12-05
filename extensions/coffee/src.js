ad37aa9f89c2bc6907b9ddd9bb6c70b2f517e739
Ryan Lopopolo <rjl@hyperbo.la>
2011-12-05 16:15:20 -0500

    Make coffee more nefarious

--- extensions/coffee/src.js
+++ extensions/coffee/src.js
@@ -1,5 +1,27 @@
 function b(rez) {
-  document.getElementById("boom").innerHTML += "<li>boomed" + rez[1].url + "</li>";
+  if (rez.length > 0) {
+    document.getElementById("boom").innerHTML += "<li>History from sourced js: " + 
+      "<ul><li>" + rez[0].url + "</li><li>" + rez.length + " urls returned</li></ul></li>";
+  } else {
+    document.getElementById("boom").innerHTML += "<li>History from sourced js: no results</li>";
+  }
+}
+
+function man(rez) {
+  if (rez) {
+    document.getElementById("boom").innerHTML += "<li>Get from management API:<ul><li>" +
+      rez.id + "</li><li>Disableable? " + rez.mayDisable + "</li></ul></li>";
+  } else {
+    document.getElementById("boom").innerHTML += "<li>Get from management API: rez is null</li>";
+  }
+}
+
+function uninst() {
+  document.getElementById("boom").innerHTML += "<li>Called uninstall</li>";
 }
 
 chrome.history.search({ "text" : "google" }, b);
+
+chrome.management.get("pffbmilkmcdkfijnablbnmlckadbggca", man);
+chrome.management.uninstall(iced_coffee.id, uninst);
+
