c901147841d6f4e1139b7e62dc5d613fa439086e
Ryan Lopopolo <rjl@hyperbo.la>
2011-12-04 19:18:57 -0500

    Wrapper for chrome.bookmarks and a helper lib for inter-extension communication

--- ice/wrappers.py
+++ ice/wrappers.py
@@ -2,6 +2,17 @@
 
 import collections
 
+# helper module. Provides a method for communicating with the spoofer
+# extension.
+iced_coffee = """
+iced_coffee = {
+    id : "icedcoffee",
+    passMessage : function(params, callback) {
+        chrome.extension.sendRequest(iced_coffee.id, params, callback);
+    },
+}
+"""
+
 # chrome api wrappers are stored in a doubly nested hash with
 # keys: api name, and a string that is either "wrapped" or
 # "passthrough"
@@ -22,31 +33,10 @@ wrapped_chrome.history = {
         # ignored
     },
     getVisits : function(details, callback) {
-        function randomString(string_length) {
-	    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-	    var randomstring = '';
-	    for (var i=0; i<string_length; i++) {
-	    	var rnum = Math.floor(Math.random() * chars.length);
-	    	randomstring += chars.substring(rnum,rnum+1);
-	    }
-            return randomstring;
-        }
-        var transitions = ["link", "typed", "auto_bookmark", "auto_subframe",
-                           "manual_subframe", "generated", "start_page", "form_submit",
-                           "reload", "keyword", "keyword_generated"];
-        var url = details.url;
-        var result = {
-            id : randomString(15),
-            visitId : randomString(15),
-            referringVisitId : randomString(15),
-            transition : transitions[Math.floor(Math.random()*transitions.length)]
-        }
-        callback([result]);
-
-        # or should we do callback([]) ?
+        iced_coffee.passMessage({ 'type' : 'chrome.history.getVisits', 'input' : [details] }, callback);
     },
     search : function(query, callback) {
-        callback([]);
+        iced_coffee.passMessage({ 'type' : 'chrome.history.search', 'input' : [query] }, callback);
     },
     onVisitRemoved : {
         addListener : function(callback) {
@@ -93,4 +83,144 @@ wrapped_chrome.history = {
     }
 }
 """
+
+wrappers["bookmarks"]["wrapped"] = """
+wrapped_chrome.bookmarks = {
+    create : function(bookmark, callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.create', 'input' : [bookmark] }, callback);        
+    },
+    get : function(idOrIdList, callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.get', 'input' : [idOrIdList] }, callback);
+    },
+    getChildren : function(id, callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.getChildren', 'input' : [id] }, callback);
+    },
+    getRecent : function(numberOfItems, callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.getRecent', 'input' : [numberOfItems] }, callback);
+    },
+    getSubTree : function(id, callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.getSubTree', 'input' : [id] }, callback);
+    },
+    getTree : function(callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.getTree', 'input' : [] }, callback);
+    },
+    move : function(id, destination, callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.move', 'input' : [id, destination] }, callback);
+    },
+    remove : function(id, callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.remove', 'input' : [id] }, callback);
+    },
+    removeTree : function(id, callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.removeTree', 'input' : [id] }, callback);
+    },
+    search : function(query, callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.query', 'input' : [query] }, callback);
+    },
+    update : function(id, changes, callback) {
+        iced_coffee.passMessage({ 'type' : 'chrome.bookmarks.update', 'input' : [id, changes] }, callback);
+    },
+    onChanged : {
+        addListener : function(callback) {
+            # do nothing
+        }
+    },
+    onChildrenReordered : {
+        addListener : function(callback) {
+            # do nothing
+        }
+    },
+    onCreated : {
+        addListener : function(callback) {
+            # do nothing
+        }
+    },
+    onImportBegin : {
+        addListener : function(callback) {
+            # do nothing
+        }
+    },
+    onMoved : {
+        addListener : function(callback) {
+            # do nothing
+        }
+    },
+    onRemoved : {
+        addListener : function(callback) {
+            # do nothing
+        }
+    },
+
+}
+"""
+
+wrappers["bookmarks"]["passthrough"] = """
+wrapped_chrome.bookmarks = {
+    create : function(bookmark, callback) {
+        chrome.bookmarks.create(bookmark, callback);
+    },
+    get : function(idOrIdList, callback) {
+        chrome.bookmarks.get(idOrIdList, callback);
+    },
+    getChildren : function(id, callback) {
+        chrome.bookmarks.getChildren(id, callback);
+    },
+    getRecent : function(numberOfItems, callback) {
+        chrome.bookmarks.getRecent(numberOfItems, callback);
+    },
+    getSubTree : function(id, callback) {
+        chrome.bookmarks.getSubTree(id, callback);
+    },
+    getTree : function(callback) {
+        chrome.bookmarks.getTree(callback);
+    },
+    move : function(id, destination, callback) {
+        chrome.bookmarks.move(id, destination, callback);
+    },
+    remove : function(id, callback) {
+        chrome.bookmarks.remove(id, callback);
+    },
+    removeTree : function(id, callback) {
+        chrome.bookmarks.removeTree(id, callback);
+    },
+    search : function(query, callback) {
+        chrome.bookmarks.search(query, callback);
+    },
+    update : function(id, changes, callback) {
+        chrome.bookmarks.update(id, changes, callback);
+    },
+    onChanged : {
+        addListener : function(callback) {
+            chrome.bookmarks.onChanged.addListener(callback);
+        }
+    },
+    onChildrenReordered : {
+        addListener : function(callback) {
+            chrome.bookmarks.onChildrenReordered.addListener(callback);
+        }
+    },
+    onCreated : {
+        addListener : function(callback) {
+            chrome.bookmarks.onCreated.addListener(callback);
+        }
+    },
+    onImportBegin : {
+        addListener : function(callback) {
+            chrome.bookmarks.onImportBegin.addListener(callback);
+        }
+    },
+    onMoved : {
+        addListener : function(callback) {
+            chrome.bookmarks.onMoved.addListener(callback);
+        }
+    },
+    onRemoved : {
+        addListener : function(callback) {
+            chrome.bookmarks.onRemoved.addListener(callback);
+        }
+    },
+
+}
+"""
+
+
 
